02d7ccaa4fae20eea62be86afb4083e68b6dc1cc
JamieAC1108 <jaaroncole2004@gmail.com>
2025-02-10 19:20:55 -0500

    final commit

--- assignment_3.py
+++ assignment_3.py
@@ -11,3 +11,22 @@ For this assignment please do the following:
 - write the output to a new file called student_grade_differences.csv
 
 '''
+with open('student_grades.csv') as f:
+    lines = f.readlines()
+
+    if len(lines) > 0:
+        grades = []
+
+        for line in lines[1:]:
+            row = line.split(',')
+            grades.append(float(row[3].replace('\n','')))
+
+        avg = sum(grades)/len(grades)
+
+        differences = []
+
+    with open('studentt_grade_differences.txt', 'w') as output_file:
+
+        for i, grade in enumerate(grades):
+            difference = grade - avg
+            output_file.write(f'Student {i + 1}: Difference from Average = {difference: .2f}\n')
